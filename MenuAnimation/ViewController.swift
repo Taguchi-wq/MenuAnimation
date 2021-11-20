632c58dae0d0d88fd66920c35da855706a550b9a
Taguchi-wq <20cm0123@jec.ac.jp>
2021-11-21 05:52:55 +0900

    collectionViewの実装

--- MenuAnimation/ViewController.swift
+++ MenuAnimation/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - @IBOutlets
+    @IBOutlet private weak var menuCollectionView: UICollectionView!
+    
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCollectionView(menuCollectionView)
     }
-
+    
+    
+    // MARK: - Methods
+    private func setupCollectionView(_ collectionView: UICollectionView) {
+        collectionView.dataSource = self
+    }
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+}
